a69a935395d07a23672a957fe54748225b07c9b1
tkhwang <tkhwang@gmail.com>
2025-06-16 01:03:19 +0900

    ♻️ refactor(header): correct naming for HeaderProjectTkcaptureBook
    - change import statement in header.tsx to reflect correct naming
    - modify function name in header-project-tkcapturebook.tsx to HeaderProjectTkcaptureBook

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { HeaderHome } from "@/components/header/header-home";
-import { HeaderProjectTkcapturebook } from "@/components/header/project/header-project-tkcapturebook";
+import { HeaderProjectTkcaptureBook } from "@/components/header/project/header-project-tkcapturebook";
 import { HeaderProjectTkCaptureBookPrivacy } from "@/components/header/project/header-project-tkcapturebook-privacy";
 import { HeaderProjectTkCaptureBookTerms } from "@/components/header/project/header-project-tkcapturebook-terms";
 import { usePathname } from "next/navigation";
@@ -15,7 +15,7 @@ export function Header() {
   const isProjectTkCaptureBookPrivacy =
     pathname === "/project/tkcapturebook/privacy";
 
-  if (isProjectTkCaptureBook) return <HeaderProjectTkcapturebook />;
+  if (isProjectTkCaptureBook) return <HeaderProjectTkcaptureBook />;
   if (isProjectTkCaptureBookTerms) return <HeaderProjectTkCaptureBookTerms />;
   if (isProjectTkCaptureBookPrivacy)
     return <HeaderProjectTkCaptureBookPrivacy />;
--- components/header/project/header-project-tkcapturebook.tsx
+++ components/header/project/header-project-tkcapturebook.tsx
@@ -1,7 +1,7 @@
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
-export function HeaderProjectTkcapturebook() {
+export function HeaderProjectTkcaptureBook() {
   return (
     <nav className="sticky top-0 z-10 bg-white border-b border-gray-200">
       <div className="container mx-auto px-4 py-4">
