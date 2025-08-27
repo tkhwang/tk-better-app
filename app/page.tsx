a27f2580a77cc44c2af4a4eccbd98e99d821239c
tkhwang <tkhwang@gmail.com>
2025-08-27 10:06:36 +0900

    ✨ feat(page): integrate Features06Page component into HomePage
    - Added `Features06Page` component to enhance the homepage with additional features.
    - Updated `HomePage` to include both `Hero06` and `Features06Page` components for a richer user experience.
    - Improved layout structure for better maintainability and visual appeal.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Features06Page from "@/components/features-06/features-06";
 import Hero06 from "@/components/hero-06/hero-06";
 
 export default function HomePage() {
   return (
-    <Hero06 />
+    <>
+      <Hero06 />
+      <Features06Page />
+    </>
   );
 }
